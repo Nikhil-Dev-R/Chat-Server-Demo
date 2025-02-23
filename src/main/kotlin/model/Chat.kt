fbfa281157bf628a4de61908b94f131295e02e3f
Nikhil Yadav <masteringprogramming2021@gmail.com>
2025-02-23 08:58:54 +0530

    Message Shared

--- src/main/kotlin/model/Chat.kt
+++ src/main/kotlin/model/Chat.kt
@@ -1,10 +1,13 @@
 package com.rudraksha.model
 
+import java.util.UUID
+
 data class Chat(
-    val id: String,
-    val name: String?,
-    val type: ChatType,
-    val participants: List<String>,
-    val createdBy: String,
-    val createdAt: Long
+    val id: String = UUID.randomUUID().toString(), // Chat id
+    val name: String?, // Chat name
+    val type: ChatType, //
+    val participants: List<String>, // All members
+    val createdBy: String, // Creator
+    val createdAt: Long = System.currentTimeMillis(), // Time
+    val messages: List<Message> = emptyList() // Messages
 )
